147951d6161f5b706f12cd78907c677d8aaf4787
Italo Gabriel <italog03@gmail.com>
2022-05-16 16:25:22 -0300

    chore: add account model protocol

--- src/domain/models/account.ts
+++ src/domain/models/account.ts
@@ -1,6 +1,9 @@
-export interface Account {
+export type Role = 'Instituição' | 'Contribuidor'
+
+export interface AccountModel {
   id: string
   name: string
   email: string
   password: string
+  role: Role
 }
