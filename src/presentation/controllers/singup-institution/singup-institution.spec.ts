3edb301234c939ca638a8d7bf40f4925fdeb63b8
Italo Gabriel <italog03@gmail.com>
2022-05-18 14:57:30 -0300

    test: ensure email is provided

--- src/presentation/controllers/singup-institution/singup-institution.spec.ts
+++ src/presentation/controllers/singup-institution/singup-institution.spec.ts
@@ -47,7 +47,7 @@ const sut = new SingUpInstitutionController(
 )
 
 describe('SingUpInstitution Controller', () => {
-  test('should return 400 if no name is not provided', async () => {
+  test('should return 400 if no name is provided', async () => {
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -63,5 +63,23 @@ describe('SingUpInstitution Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
+
+  test('should return 400 if no email is provided', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        type: 'valid_type',
+        cnpj: 'valid_cnpj',
+        cellphone: 'valid_cellphone',
+        telephone: 'valid_telephone'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
