012f0e2d624873bf0678adf6a5ad10b846fd4632
Italo Gabriel <italog03@gmail.com>
2022-05-20 08:41:10 -0300

    test: ensure EmailValidator calls with correct email

--- src/presentation/controllers/singup-institution/singup-institution.spec.ts
+++ src/presentation/controllers/singup-institution/singup-institution.spec.ts
@@ -258,4 +258,24 @@ describe('SingUpInstitution Controller', () => {
       new InvalidParamError('email')
     )
   })
+
+  test('should call EmailValidator eith correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        type: 'valid_type',
+        cnpj: 'valid_cnpj',
+        cellphone: 'valid_cellphone',
+        telephone: 'valid_telephone'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+  })
 })
