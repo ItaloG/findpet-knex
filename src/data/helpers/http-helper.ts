a9bf20078f0fdfd8fdc27bf7fffc91417d25b89e
Italo Gabriel <italog03@gmail.com>
2022-05-24 15:20:23 -0300

    chore: remove files

--- src/data/helpers/http-helper.ts
+++
@@ -1,7 +0,0 @@
-import { UserExistsError } from '../errors/user-exists-error'
-import { HttpResponse } from '../protocols/http'
-
-export const serverConflict = (): HttpResponse => ({
-  statusCode: 409,
-  body: new UserExistsError()
-})
