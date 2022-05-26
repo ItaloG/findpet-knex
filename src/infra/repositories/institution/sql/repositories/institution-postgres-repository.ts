ac431367bf94c9f924bda4ac0541216d3425f8df
Italo Gabriel <italog03@gmail.com>
2022-05-26 15:13:36 -0300

    feat: add FindInstitutionByEmailRepository

--- src/infra/repositories/institution/sql/repositories/institution-postgres-repository.ts
+++ src/infra/repositories/institution/sql/repositories/institution-postgres-repository.ts
@@ -1,11 +1,13 @@
 import { AddInstitutionAccountRepository, AddInstitutionAccountWithPositionModel } from '../../../../../data/protocols/institution/add-institution-account-repository'
+import { FindInstitutionByEmailRepository } from '../../../../../data/protocols/institution/find-institution-by-email-repository'
 import { AddAccountModel } from '../../../../../domain/models/account'
 import { InstitutionModel } from '../../../../../domain/models/institution'
 import { db } from '../../../../knex/connection'
 
-export class InstitutionPostgresRepository implements AddInstitutionAccountRepository {
+export class InstitutionPostgresRepository implements AddInstitutionAccountRepository, FindInstitutionByEmailRepository {
     private readonly table = 'institutions'
     private readonly accountTable = 'accounts'
+
     async add (institutionData: AddInstitutionAccountWithPositionModel): Promise<InstitutionModel> {
       try {
         const [account] = await db<AddAccountModel & {id: string}>(this.accountTable).insert({ email: institutionData.email, name: institutionData.name, password: institutionData.password, role: 'Instituição' }).returning('*')
@@ -15,5 +17,11 @@ export class InstitutionPostgresRepository implements AddInstitutionAccountRepos
       } catch (error: any) {
         throw new Error(error.message)
       }
+    }
+
+    async find (email: string): Promise<InstitutionModel | null> {
+      const user = await db<InstitutionModel>(this.table).where(email).first()
+      if (!user) return null
+      return user
     }
 }
