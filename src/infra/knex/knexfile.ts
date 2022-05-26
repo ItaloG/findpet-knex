17b8949f2e741c5d38d5ca5af3440962f9e3e33d
Italo Gabriel <italog03@gmail.com>
2022-05-26 13:27:58 -0300

    feat: add knex connection file

---
+++ src/infra/knex/knexfile.ts
@@ -0,0 +1,20 @@
+// Update with your config settings.
+import dotenv from 'dotenv'
+import { resolve } from 'path'
+
+const dotenvFilePath = resolve(__dirname, '..', '..', '..', '.env')
+dotenv.config({ path: dotenvFilePath })
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    }
+  }
+}
