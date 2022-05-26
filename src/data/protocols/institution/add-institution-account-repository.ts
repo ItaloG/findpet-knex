a76365a7a48736d5c09cbb961805fa85ec07fdd2
Italo Gabriel <italog03@gmail.com>
2022-05-26 14:18:40 -0300

    feat: add AddInstitutionAccountWithPositionModel interface

--- src/data/protocols/institution/add-institution-account-repository.ts
+++ src/data/protocols/institution/add-institution-account-repository.ts
@@ -1,6 +1,25 @@
-import { InstitutionModel } from '../../../domain/models/institution'
-import { AddInstitutionAccountModel } from '../../../domain/usecases/add-institution-account'
+import { CEP } from '../../../domain/models/cep'
+import { InstitutionModel, InstitutionType } from '../../../domain/models/institution'
+
+export interface AddInstitutionAccountWithPositionModel {
+  name: string
+  email: string
+  password: string
+  type: InstitutionType
+  cnpj: string
+  description: string
+  cellphone?: number
+  telephone?: number
+  street: string
+  cep: CEP
+  city: string
+  state: string
+  number: number
+  complement?: string
+  lat: string
+  lng: string
+}
 
 export interface AddInstitutionAccountRepository {
-  add: (institutionData: AddInstitutionAccountModel) => Promise<InstitutionModel>
+  add: (institutionData: AddInstitutionAccountWithPositionModel) => Promise<InstitutionModel>
 }
