a17c7e045691e386b01788b10b83fc62194e012e
Italo Gabriel <italog03@gmail.com>
2022-05-26 15:20:31 -0300

    refactor: change methods names

--- src/data/protocols/institution/find-institution-by-cnpj-repository.ts
+++ src/data/protocols/institution/find-institution-by-cnpj-repository.ts
@@ -1,5 +1,5 @@
 import { InstitutionModel } from '../../../domain/models/institution'
 
 export interface FindInstitutionByCnpjRepository {
-  find: (cnpj: string) => Promise<InstitutionModel | null>
+  findByCnpj: (cnpj: string) => Promise<InstitutionModel | null>
 }
