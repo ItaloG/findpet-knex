21a6cfa6014797da09cc4e76cc3d30eb2ef7bff5
Italo Gabriel <italog03@gmail.com>
2022-05-26 15:12:32 -0300

    fix: add nul return

--- src/data/protocols/institution/find-institution-by-email-repository.ts
+++ src/data/protocols/institution/find-institution-by-email-repository.ts
@@ -1,5 +1,5 @@
 import { InstitutionModel } from '../../../domain/models/institution'
 
 export interface FindInstitutionByEmailRepository {
-  find: (email: string) => Promise<InstitutionModel>
+  find: (email: string) => Promise<InstitutionModel | null>
 }
