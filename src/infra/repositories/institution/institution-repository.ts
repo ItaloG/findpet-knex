2fa6e09281a7a5438487e12b866a03ff9ed8bc7a
Italo Gabriel <italog03@gmail.com>
2022-05-26 15:17:50 -0300

    refactor: facility FindInstitutionByEmailRepository export

--- src/infra/repositories/institution/institution-repository.ts
+++ src/infra/repositories/institution/institution-repository.ts
@@ -1,10 +1,13 @@
 import { AddInstitutionAccountRepository } from '../../../data/protocols/institution/add-institution-account-repository'
+import { FindInstitutionByEmailRepository } from '../../../data/protocols/institution/find-institution-by-email-repository'
 import { InstitutionPostgresRepository } from './sql/repositories/institution-postgres-repository'
 
 const institutionPostgresRepository = new InstitutionPostgresRepository()
 
 const addInstitutionAccountRepository: AddInstitutionAccountRepository = institutionPostgresRepository
+const findInstitutionByEmailRepository: FindInstitutionByEmailRepository = institutionPostgresRepository
 
 export {
-  addInstitutionAccountRepository
+  addInstitutionAccountRepository,
+  findInstitutionByEmailRepository
 }
