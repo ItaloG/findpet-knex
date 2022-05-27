5c4fdc9586d9a7975bbcaf09f3e77e39bd9222d7
Italo Gabriel <italog03@gmail.com>
2022-05-27 15:24:30 -0300

    feat: add googleGeolocationApi implementation

---
+++ src/infra/apis/google-geolocation-api.ts
@@ -0,0 +1,25 @@
+import { FindGeolocation, Position } from '../../data/protocols/find-geolocation'
+import { CEP } from '../../domain/models/cep'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import { resolve } from 'path'
+
+const dotenvFilePath = resolve(__dirname, '..', '..', '..', '.env-development')
+dotenv.config({ path: dotenvFilePath })
+
+export class GoogleGeolocationApi implements FindGeolocation<CEP> {
+  async find (arg: CEP): Promise<Position> {
+    const response = await axios({
+      method: 'post',
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?',
+      params: {
+        address: arg.cep,
+        key: process.env.GOOGLE_API_KEY
+      }
+    })
+    return {
+      lat: response.data.results[0].geometry.location.lat,
+      lng: response.data.results[0].geometry.location.lng
+    }
+  }
+}
