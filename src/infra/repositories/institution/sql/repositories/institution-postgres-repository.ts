cd9038c76c33bcd073bda0b3c8d2c43cc5233f0e
Italo Gabriel <italog03@gmail.com>
2022-05-31 10:51:02 -0300

    fix: correct database insert

--- src/infra/repositories/institution/sql/repositories/institution-postgres-repository.ts
+++ src/infra/repositories/institution/sql/repositories/institution-postgres-repository.ts
@@ -1,7 +1,7 @@
 import { AddInstitutionAccountRepository, AddInstitutionAccountWithPositionModel } from '../../../../../data/protocols/institution/add-institution-account-repository'
 import { FindInstitutionByCnpjRepository } from '../../../../../data/protocols/institution/find-institution-by-cnpj-repository'
 import { FindInstitutionByEmailRepository } from '../../../../../data/protocols/institution/find-institution-by-email-repository'
-import { AddAccountModel } from '../../../../../domain/models/account'
+import { AccountModel, AddAccountModel } from '../../../../../domain/models/account'
 import { InstitutionModel } from '../../../../../domain/models/institution'
 import { db } from '../../../../knex/connection'
 
@@ -12,8 +12,7 @@ export class InstitutionPostgresRepository implements AddInstitutionAccountRepos
     async add (institutionData: AddInstitutionAccountWithPositionModel): Promise<InstitutionModel> {
       try {
         const [account] = await db<AddAccountModel & {id: string}>(this.accountTable).insert({ email: institutionData.email, name: institutionData.name, password: institutionData.password, role: 'Instituição' }).returning('*')
-        const [institution] = await db<AddInstitutionAccountWithPositionModel>(this.table).insert(institutionData).returning('*') // mudar
-        console.log(Object.assign({}, institution, { account_id: account.id }))
+        const [institution] = await db<InstitutionModel>(this.table).insert({ account_id: account.id, cellphone: institutionData.cellphone, cep: institutionData.cep, city: institutionData.city, cnpj: institutionData.cnpj, complement: institutionData.complement, description: institutionData.description, lat: institutionData.lat, lng: institutionData.lng, number: institutionData.number, state: institutionData.state, street: institutionData.street, telephone: institutionData.telephone, type: institutionData.type }).returning('*') // mudar
         return Object.assign({}, institution, { account_id: account.id })
       } catch (error: any) {
         throw new Error(error.message)
@@ -21,13 +20,15 @@ export class InstitutionPostgresRepository implements AddInstitutionAccountRepos
     }
 
     async findByEmail (email: string): Promise<InstitutionModel | null> {
-      const user = await db<InstitutionModel>(this.table).where(email).first()
+      const user = await db<AccountModel>(this.accountTable).where('email', email).first()
       if (!user) return null
-      return user
+      const institution = await db<InstitutionModel>(this.table).where('account_id', user.id).first()
+      if (!institution) return null
+      return institution
     }
 
     async findByCnpj (cnpj: string): Promise<InstitutionModel | null> {
-      const user = await db<InstitutionModel>(this.table).where(cnpj).first()
+      const user = await db<InstitutionModel>(this.table).where('cnpj', cnpj).first()
       if (!user) return null
       return user
     }
